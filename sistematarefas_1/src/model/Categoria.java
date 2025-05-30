371a90f81ab8754144aca0fe3a56d81d07832bd9
devdiiego <156592819+devdiiego@users.noreply.github.com>
2025-05-30 08:33:35 -0300

    Update Categoria.java
    Comente o código

--- sistematarefas_1/src/model/Categoria.java
+++ sistematarefas_1/src/model/Categoria.java
@@ -1,25 +1,22 @@
 package src.model;
+
+// Classe que representa uma categoria
 public class Categoria {
     private int id;
     private String nome;
 
+    // Construtor
     public Categoria(int id, String nome) {
         this.id = id;
         this.nome = nome;
     }
 
-    public int getId() {
-        return id;
-    }
+    // Getters e setter
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
+    // Representação em texto
     @Override
     public String toString() {
         return "Categoria [ID=" + id + ", Nome=" + nome + "]";
