a97e396ab5e6bd614325423702bac313cc18c428
devdiiego <156592819+devdiiego@users.noreply.github.com>
2025-05-30 08:32:17 -0300

    Update Colaborador.java
    Código comentado

--- sistematarefas_1/src/model/Colaborador.java
+++ sistematarefas_1/src/model/Colaborador.java
@@ -1,11 +1,14 @@
 package src.model;
 
+// A classe Colaborador herda os atributos e métodos da classe Entidade
 public class Colaborador extends Entidade {
 
+    // Construtor que inicializa o colaborador com id e nome
     public Colaborador(int id, String nome) {
-        super(id, nome);
+        super(id, nome); // Chama o construtor da classe Entidade
     }
 
+    // Sobrescreve o toString para exibir as informações do colaborador
     @Override
     public String toString() {
         return "Colaborador [ID=" + id + ", Nome=" + nome + "]";
